8ace97db3d3a030c780ee4a72896faf0b688bee4
Androidmini9x <androidmini9x@gmail.com>
2022-12-09 01:40:09 +0300

    Add: 0x00 day of low code

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,13 @@
 #include <stdio.h>
 /**
+ * main - Entry point of app
  * 0x00 hello world
  * Return: 0
  */
 int main(void)
 {
 	char hello[] = "\"Programming is like building a multilingual puzzle";
+
 	puts(hello);
-	return 0;
+	return (0);
 }
--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,7 +1,11 @@
 #include <stdio.h>
-
+/**
+ * main - Entry point of app
+ * 0x00 hello world
+ * Return: 0
+ */
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
-	return 0;
+	return (0);
 }
