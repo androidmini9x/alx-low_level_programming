6c27ce4d27c5a674eca0096772dded310ff7f5bf
Androidmini9x <androidmini9x@gmail.com>
2023-01-06 19:22:07 +0300

    Solve 0x0A

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
 /**
  * main - adds positive numbers
  * @argc: number of args
@@ -8,38 +10,32 @@
  */
 int main(int argc, char *argv[])
 {
-	int i, j, sum;
-	char *str;
+	int i, j, length, sum;
+	char *ptr;
 
-	if (argc == 1)
+	if (argc < 2)
+	printf("0\n");
+	else
 	{
-		printf("0\n");
-		return (0);
-	}
-
-	for (i = 1; i < argc; i++)
-	{
-		int num;
-
-		str = argv[i];
-		j = 0;
-
-		while (*(str + j) != '\0')
+		sum = 0;
+		for (i = 1; i < argc; i++)
 		{
-			char c = *(str + j);
+			ptr = argv[i];
+			length = strlen(ptr);
 
-			if (c > '9' || c < '0')
+			for (j = 0; j < length; j++)
 			{
-				printf("Error\n");
-				return (1);
+				if (isdigit(*(ptr + j)) == 0)
+				{
+					printf("Error\n");
+					return (1);
+				}
 			}
-			j++;
+
+			sum += atoi(argv[i]);
 		}
 
-		num = atoi(argv[i]);
-		sum += num;
+		printf("%d\n", sum);
 	}
-	printf("%d\n", sum);
-
 	return (0);
 }
