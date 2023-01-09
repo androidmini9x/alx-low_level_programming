21627df21195dd1682ea2d0dd442b59511ef9ef7
Androidmini9x <androidmini9x@gmail.com>
2023-01-10 01:43:33 +0300

    Fix some bugs

---
+++ 0x0B-malloc_free/100-argstostr.c
@@ -0,0 +1,46 @@
+#include "main.h"
+#include <stdlib.h>
+#include <string.h>
+/**
+ * argstostr - concatenates all the arguments of your program.
+ * @ac: int
+ * @av: char[][]
+ * Return: char
+ */
+char *argstostr(int ac, char **av)
+{
+	int i;
+	int j;
+	int length;
+	char *str;
+
+	if (ac == 0 || av == NULL)
+		return (0);
+
+	length = 0;
+	for (i = 0; i < ac; i++)
+		length += strlen(av[i]);
+
+	str = malloc(sizeof(char) * length + ac);
+
+	if (str == NULL)
+		return (0);
+
+	for (i = 0; i < ac; i++)
+	{
+		int k = 0;
+
+		while (*(av[i] + k) != '\0')
+		{
+			*(str + j) = *(av[i] + k);
+			j++;
+			k++;
+		}
+		*(str + j) = '\n';
+		j++;
+	}
+
+	*(str + j) = '\0';
+
+	return (str);
+}
