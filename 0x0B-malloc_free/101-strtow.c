306e80fe11810da64823ee31142e3f5f1c377af8
Androidmini9x <androidmini9x@gmail.com>
2023-01-10 03:05:24 +0300

    Add bonus tasks

---
+++ 0x0B-malloc_free/101-strtow.c
@@ -0,0 +1,80 @@
+#include "main.h"
+#include <stdlib.h>
+#include <string.h>
+/**
+ * strtow - splits a string into words;
+ * @str: string
+ * Return: char
+ */
+char **strtow(char *str)
+{
+	int i;
+	int j;
+	int k;
+	int c;
+	int word_count = 0;
+	int word_len;
+	char **str_arr;
+
+	if (str == NULL || strcmp(str, "") == 0)
+		return (0);
+
+	while (*(str + i) != '\0')
+	{
+		if (*(str + i) != ' ')
+		{
+			while (*(str + i) != '\0' && *(str + i) != ' ')
+			{
+				i++;
+			}
+			word_count++;
+			i--;
+		}
+		i++;
+	}
+
+	str_arr = malloc(sizeof(char *) * word_count);
+
+	if (str == NULL)
+		return (0);
+
+	i = 0;
+	j = 0;
+
+	while (*(str + i) != '\0')
+	{
+		if (*(str + i) != ' ')
+		{
+			word_len = 0;
+			while (*(str + i) != '\0' && *(str + i) != ' ')
+			{
+				i++;
+				word_len++;
+			}
+
+			str_arr[j] = malloc(sizeof(char) * word_len + 1);
+
+			if (str_arr[j] == NULL)
+			{
+				for (i = 0; i < j; i++)
+					free(str_arr[j]);
+				free(str_arr);
+
+				return (0);
+			}
+
+			c = 0;
+			for (k = i - word_len; k < i; k++)
+			{
+				str_arr[j][c++] = *(str + k);
+			}
+			str_arr[j][c] = '\0';
+
+			j++;
+			i--;
+		}
+		i++;
+	}
+
+	return (str_arr);
+}
--- 0x0B-malloc_free/main.c
+++ 0x0B-malloc_free/main.c
@@ -1,23 +1,38 @@
+#include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
 
-char *argstostr(int, char **);
+/**
+ * print_tab - Prints an array of string
+ * @tab: The array to print
+ *
+ * Return: nothing
+ */
+void print_tab(char **tab)
+{
+    int i;
+
+    for (i = 0; tab[i] != NULL; ++i)
+    {
+        printf("%s\n", tab[i]);
+    }
+}
 
 /**
- * main - check the code
+ * main - check the code for ALX School students.
  *
- * Return: Always 0.
+ * Return: 1 if an error occurred, 0 otherwise
  */
-int main(int ac, char *av[])
+int main(void)
 {
-	char *s;
+    char **tab;
 
-	s = argstostr(ac, av);
-	if (s == NULL)
-	{
-		return (1);
-	}
-	printf("%s", s);
-	free(s);
-	return (0);
+    tab = strtow("    ALX   School         #cisfun a");
+    if (tab == NULL)
+    {
+        printf("Failed\n");
+        return (1);
+    }
+    print_tab(tab);
+    return (0);
 }
