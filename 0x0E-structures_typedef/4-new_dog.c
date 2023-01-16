87b0b9d1f515e8ce01a860b12ddaa2be10c4fb25
Androidmini9x <androidmini9x@gmail.com>
2023-01-17 00:00:51 +0300

    Fix: 0x0E Tasks

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,4 +1,5 @@
 #include <stdlib.h>
+#include <string.h>
 #include "dog.h"
 /**
  * new_dog - creates a new dog
@@ -10,6 +11,7 @@
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *n_dog;
+	int i = 0;
 
 	if (name == NULL || age < 0 || owner == NULL)
 		return (NULL);
@@ -18,9 +20,24 @@ dog_t *new_dog(char *name, float age, char *owner)
 	if (n_dog == NULL)
 		return (NULL);
 
-	n_dog->name = name;
+	n_dog->name = malloc(strlen(name) + 1);
+	if (n_dog->name == NULL)
+		return (NULL);
+	while (*(name + i) != '\0')
+	{
+		*(n_dog->name + i) = *(name + i);
+		i++;
+	}
+	*(n_dog->name + i) = '\0';
 	n_dog->age = age;
-	n_dog->owner = owner;
-
+	n_dog->owner = malloc(strlen(name) + 1);
+	if (n_dog->owner == NULL)
+		return (NULL);
+	i = 0;
+	while (*(owner + i) != '\0')
+	{
+		*(n_dog->owner + i) = *(owner + i);
+		i++;
+	}
 	return (n_dog);
 }
