921f04d30bd12249aef980d125df84cee5e105e6
Androidmini9x <androidmini9x@gmail.com>
2023-01-18 20:09:15 +0300

    Fixed: Th-Task

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -23,7 +23,7 @@ int main(int argc, char **argv)
 	num2 = atoi(argv[3]);
 	op = argv[2];
 
-	if (argv[2] == NULL || argv[2][1] != '\0')
+	if (argv[2] == NULL || *(argv[2] + 1) != '\0')
 	{
 		printf("Error\n");
 		exit(99);
