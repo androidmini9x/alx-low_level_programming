752bafc4c70b0ec44abb1822233f5193ef03125a
Androidmini9x <androidmini9x@gmail.com>
2023-01-18 18:55:27 +0300

    Fix: Some Bugs

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -10,7 +10,7 @@
  */
 int main(int argc, char **argv)
 {
-	int num1, num2;
+	int num1, num2, result;
 	char *op;
 	int (*func)(int, int);
 
@@ -24,12 +24,6 @@ int main(int argc, char **argv)
 	num2 = atoi(argv[3]);
 	op = argv[2];
 
-	if ((*op == '/' || *op == '%') && num2 == 0)
-	{ /* divide by zero exception */
-		printf("Error\n");
-		return (100);
-	}
-
 	func = get_op_func(op);
 
 	if (func == NULL || strlen(op) > 1)
@@ -38,7 +32,8 @@ int main(int argc, char **argv)
 		exit(99);
 	}
 
-	printf("%d\n", func(num1, num2));
+	result = func(num1, num2);
+	printf("%d\n", result);
 
 	return (0);
 }
