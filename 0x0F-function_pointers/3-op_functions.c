b3b00bd47ccbaaeb4e3d0c536e5ba213a91afe7e
Androidmini9x <androidmini9x@gmail.com>
2023-01-18 20:24:08 +0300

    Fixed: Th-Task

--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -1,4 +1,5 @@
-#include "3-calc.h"
+#include <stdio.h>
+#include <stdlib.h>
 /**
  * op_add - returns the sum of a and b
  * @a: num1
@@ -37,6 +38,11 @@ int op_mul(int a, int b)
  */
 int op_div(int a, int b)
 {
+	if (b == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
 	return (a / b);
 }
 /**
@@ -47,5 +53,10 @@ int op_div(int a, int b)
  */
 int op_mod(int a, int b)
 {
+	if (b == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
 	return (a % b);
 }
