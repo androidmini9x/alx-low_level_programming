2b8049100e73e3ab3f02ac15da254da8222d3048
Androidmini9x <androidmini9x@gmail.com>
2023-01-19 20:44:11 +0300

    Solved: 0x10

---
+++ 0x10-variadic_functions/2-print_strings.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdarg.h>
+#include "variadic_functions.h"
+/**
+ * print_strings - print string
+ * @separator: chars
+ * @n: int
+ * Return: Void
+ */
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+	va_list ap;
+	unsigned int i;
+	char *str;
+
+	if (separator == NULL)
+		separator = "";
+	va_start(ap, n);
+	for (i = 0; i < n; i++)
+	{
+		str = va_arg(ap, char*);
+		if (i > 0)
+			printf("%s", separator);
+		printf("%s", str);
+	}
+	printf("\n");
+
+	va_end(ap);
+}
