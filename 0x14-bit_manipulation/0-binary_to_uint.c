42fcad341a897f73954edf43400ca377589660d5
Androidmini9x <androidmini9x@gmail.com>
2023-02-03 04:04:57 +0300

    Solve: 0x14 tasks

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,6 +1,4 @@
-#include <string.h>
 #include "main.h"
-unsigned int power(int number, int e);
 /**
  * binary_to_uint - converts a binary number to an unsigned int.
  * @b: binary as string
@@ -8,44 +6,23 @@ unsigned int power(int number, int e);
  */
 unsigned int binary_to_uint(const char *b)
 {
-	int len;
+	unsigned int i = 0;
 	unsigned int num = 0;
-	int i = 0;
 
-	if (b == NULL)
-		return (0);
 
-	len = strlen(b) - 1;
-
-	while (len >= 0)
+	while (*(b + i) != '\0')
 	{
-		if (*(b + len) == '1')
-		{
-			num += power(2, i);
-		}
-		if (*(b + len) != '1' && *(b + len) != '0')
+		if (*(b + i) != '1' && *(b + i) != '0')
 			return (0);
+
+		num = num << 1;
+		if (*(b + i) == '1')
+		{
+			num ^= 1;
+		}
+
 		i++;
-		len--;
 	}
 
 	return (num);
-}
-/**
- * power - power of number.
- * @number: binary as string
- * @e: power
- * Return: unsigned int
- */
-unsigned int power(int number, int e)
-{
-	int i;
-	unsigned int result = 1;
-
-	for (i = 0; i < e; i++)
-	{
-		result *= number;
-	}
-
-	return (result);
 }
