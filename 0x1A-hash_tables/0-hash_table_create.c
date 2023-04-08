a39e5890497a94287c8ee5f30a3dcc8451d008dd
Androidmini9x <androidmini92@gmail.com>
2023-04-08 05:19:55 +0300

    Solve: 0x1A

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -1,9 +1,8 @@
 #include "hash_tables.h"
 /**
- * hash_table_create - Creates a hash table.
- * @size: The size of the array.
- *
- * Return: Hashtable
+ * hash_table_create - Creates a hash table
+ * @size: The size of the array
+ * Return: hash_table_t
  */
 hash_table_t *hash_table_create(unsigned long int size)
 {
